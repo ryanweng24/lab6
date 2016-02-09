9c5db4cb063a1d244dae49476b7bc88bb945dca5
Ryan Weng <ryanweng24@gmail.com>
2016-02-09 16:59:34 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,6 +5,17 @@ $(document).ready(function() {
 	initializePage();
 })
 
+$(".project a").click(function(e) {
+	var projectID = $(this).closest('.project').attr('id');
+	var idNumber = projectID.substr('project'.length);
+	$.get("/project/" + idNumber, addProject);
+	console.log("/project/" + idNumber);
+})
+
+$('#colorBtn').click(function(e) {
+	$.get("/palette", addColor);
+})
+
 /*
  * Function that is called when the document is ready.
  */
@@ -12,6 +23,22 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+}
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage img">' + '<h3>' + result['date'] + '</h3>' + result['summary']
+	$(".details").html(projectHTML);
+}
+
+function addColor(result) {
+	var colorArray = result['colors'];
+	var colors = colorArray['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
 
 /*
